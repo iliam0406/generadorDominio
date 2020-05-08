df6ecf357111a792a80122bc4f3b9b8b64b00d6a
iliam0406 <iliam.carrion@gmail.com>
2020-05-08 18:58:17 -0400

    Generador de dominio v1.0

---
+++ for.js
@@ -0,0 +1,14 @@
+var pronoun = ['the','our'];
+var adj = ['great', 'big' ];
+var noun = ['jogger','racoon'];
+let i = 0; let j=0; let k =0;
+
+for (i = 0; i < pronoun.length; i++) {
+    for (j = 0; j < adj.length; j++) {
+        for (k = 0; k < noun.length; k++) {
+            const element = pronoun[i] + adj[j] + noun[k];
+           console.log('https://www.' + element + '.com') ;
+        } 
+    }
+}
+

30780746b741045a041c5ef6ad203216ee92bc93
iliam0406 <iliam.carrion@gmail.com>
2020-05-09 19:09:55 -0400

    actualización con función y estilo CSS

--- for.js
+++ for.js
@@ -2,13 +2,24 @@ var pronoun = ['the','our'];
 var adj = ['great', 'big' ];
 var noun = ['jogger','racoon'];
 let i = 0; let j=0; let k =0;
+let resultado = '';
+let ext = ''
 
-for (i = 0; i < pronoun.length; i++) {
-    for (j = 0; j < adj.length; j++) {
-        for (k = 0; k < noun.length; k++) {
-            const element = pronoun[i] + adj[j] + noun[k];
-           console.log('https://www.' + element + '.com') ;
-        } 
+function accion(){
+    
+    for (i = 0; i < pronoun.length; i++) {
+        for (j = 0; j < adj.length; j++) {
+            for (k = 0; k < noun.length; k++) {
+                const element = pronoun[i] + adj[j] + noun[k]; 
+               //console.log('https://www.' + element + '.com') ;
+               ext = document.getElementsByName("inputoptiene")[0].value;
+               resultado += 'www.' + element + '.' + ext + '<br />' 
+            } 
+        }
     }
+   
+    document.querySelector("#respuesta").innerHTML = resultado;
 }
+
+
 
